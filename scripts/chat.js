3996e6b5bd3b951e7759ef4de1f5cad0de77796d
Joshua Spence <joshua-spence@outlook.com>
2020-07-31 14:13:29 +0100

    new files

---
+++ scripts/chat.js
@@ -0,0 +1,48 @@
+
+class Chatroom {
+    constructor(room, username){
+        this.room = room;
+        this.username = username;
+        this.chats = db.collection('chats');
+        this.unsub;
+    }
+    async addChat(message){
+        //format a chat object 
+        const now = new Date();
+        const chat = {
+            message,
+            username: this.username,
+            room: this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(now)
+        };
+        //save the document
+        const response = await this.chats.add(chat);
+        return response;
+    }
+    //realTime Listener
+    getChats(callback){
+        this.chats
+            .where('room', '==', this.room)
+            .orderBy('created_at')
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if(change.type === 'added'){
+                        //update the UI
+                        callback(change.doc.data());
+                    }
+                });
+            });
+    }
+
+    updateName(username){
+        this.username = username;
+    }
+    updateRoom(room){
+        this.room = room;
+        console.log('room updated');
+        if(this.unsub){
+            this.unsub();
+        }
+    }
+}
+
